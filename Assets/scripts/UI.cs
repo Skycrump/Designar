637a12e3bccca6a0d52f79560e95458287d726c1
ainiinpain <s10247642@connect.np.edu.sg>
2024-06-18 18:32:53 +0800

    fix bugs
    fixed when item is placed with the item is placed, it will duplicate it self multiple times when pressed and added a "Set" premade for users

--- Assets/scripts/UI.cs
+++ Assets/scripts/UI.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class UI : MonoBehaviour
 {
+    public GameObject targetObject;
+
+    public void TransferToTarget()
+    {
+        if (targetObject.GetComponent<MeshRenderer>().isVisible)
+        {
+            transform.SetParent(targetObject.transform.parent, false);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
