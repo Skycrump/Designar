0e294da705ca835a7e2b824aabbea37d33fa1641
ainiinpain <s10247642@connect.np.edu.sg>
2024-07-10 12:03:36 +0800

    measure AR
    debugged the measure AR and have yet to test

--- Assets/scripts/X_Text.cs
+++ Assets/scripts/X_Text.cs
@@ -4,9 +4,8 @@ using TMPro;
 
 public class X_Text : MonoBehaviour
 {
-
     public GameObject GamePose1, GamePose2;
-    public GameObject LenthX;
+    public GameObject LengthX;
     public Vector3 position2, position1;
     Vector3 position_difference, pos_temp, size_temp;
     float C, B;
@@ -16,12 +15,12 @@ public class X_Text : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-       textMeshPro = GetComponent<TextMeshPro>();
-       if (textMeshPro == null)
-       {
-           Debug.LogError("TextMeshPro component not found on " + gameObject.name);
-           return;
-       }
+        textMeshPro = GetComponent<TextMeshPro>();
+        if (textMeshPro == null)
+        {
+            Debug.LogError("TextMeshPro component not found on " + gameObject.name);
+            return;
+        }
     }
 
     // Update is called once per frame
@@ -34,15 +33,18 @@ public class X_Text : MonoBehaviour
 
         float distance = position2.x - position1.x;
         //distance = Mathf.RoundToInt(distance);
-        //GetComponent<TextMesh>().text = distance.ToString("f1");
+        GetComponent<TextMeshPro>().text = distance.ToString("f1");
         textMeshPro.text = distance.ToString("f1");
 
+        //the lines below might be the issue
 
-        pos_temp = LenthX.GetComponent<LinkPoseX>().pos_tempX;
-        pos_temp.x = pos_temp.x + distance/2;
+        pos_temp = LengthX.GetComponent<LinkPoseX>().pos_tempX;
+        pos_temp.x = pos_temp.x + distance / 2;
         transform.position = pos_temp;
 
+        //code out the midpoint formula for the 2 text and find mid point to put the ui in it
+
+        //position1 - position2 (midpoint)
+        //text.transform.position
     }
-
-
 }
